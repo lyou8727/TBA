819c1203583189986c377282f92cf31dfeae2e9b
Leon <Leon@Loen>
2017-11-06 00:48:04 -0500

    boss

--- TBA/src/room/bossRoom.java
+++ TBA/src/room/bossRoom.java
@@ -11,21 +11,21 @@ public abstract class bossRoom extends Room{
 	public bossRoom(int x, int y) {
 		super(x, y);
 	}
-	
+	//gets health
 	public int getHealth()
 	{
 		Random rand = new Random();
 		int  n = rand.nextInt(30) + 25;
 		return bossHealth = n;	
 	}
-	
+	//gets attack
 	public int getAttack()
 	{
 		Random rand = new Random();
 		int  n = rand.nextInt(13) + 10;
 		return bossAttack = n;
 	}
-	
+	//gets defense
 	public int getDefense()
 	{
 		Random rand = new Random();
